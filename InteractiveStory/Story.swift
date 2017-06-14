7dd18c84d4f7366cb6dd71ceead2e697a1e1b769
Nate Bird <natebird@gmail.com>
2017-06-14 11:39:50 -0400

    Add Story enum

---
+++ InteractiveStory/Story.swift
@@ -0,0 +1,21 @@
+//
+//  Story.swift
+//  InteractiveStory
+//
+//  Created by Nate Bird on 6/14/17.
+//  Copyright © 2017 Birdhouse. All rights reserved.
+//
+
+import Foundation
+
+enum Story {
+    case returnTrip
+    case touchDown
+    case homeward
+    case rover
+    case cave
+    case crate
+    case monster
+    case droid
+    case home
+}
