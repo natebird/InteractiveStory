168fea23dba99dc9b5fe584e301ff8a0f999aaf5
Nate Bird <natebird@gmail.com>
2017-06-15 08:20:23 -0400

    Add adventure struct

--- InteractiveStory/Page.swift
+++ InteractiveStory/Page.swift
@@ -47,3 +47,39 @@ extension Page {
         return page
     }
 }
+
+struct Adventure {
+    static var story: Page {
+        let returnTrip = Page(story: .returnTrip)
+        let touchdown = returnTrip.addChoiceWith(title: "Stop and Investigate", story: .touchDown)
+        let homeward = returnTrip.addChoiceWith(title: "Continue home to Earth", story: .homeward)
+        let rover = touchdown.addChoiceWith(title: "Explore the Rover", story: .rover)
+        let crate = touchdown.addChoiceWith(title: "Open the Crate", story: .crate)
+
+        homeward.addChoiceWith(title: "Head back to Mars", page: touchdown)
+        let home = homeward.addChoiceWith(title: "Continue Home to Earth", story: .home)
+
+        let cave = rover.addChoiceWith(title: "Explore the Coordinates", story: .cave)
+        rover.addChoiceWith(title: "Return to Earth", page: home)
+
+        cave.addChoiceWith(title: "Continue towards faint light", story: .droid)
+        cave.addChoiceWith(title: "Refill the ship and explore the rover", page: rover)
+
+        crate.addChoiceWith(title: "Explore the Rover", page: rover)
+        crate.addChoiceWith(title: "Use the key", story: .monster)
+
+        return returnTrip
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
